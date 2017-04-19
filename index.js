4da7be0934216491e3ad4a73442d89f945a5835c
Scott Olsen <scottolsen@Scotts-MacBook-Pro.local>
2017-04-19 00:15:53 -0400

    Work on help functionality

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 #!/usr/bin/env node
 const handler = require('./lib/handler.js');
 const argvs = process.argv;
-console.log("!");
+const fs = require('fs');
+const path = require('path');
+global.rootDir = path.resolve(__dirname);
+
+//grab version
+let pkgf = fs.readFileSync('./package.json');
+let pkgj = JSON.parse(pkgf); 
+
+console.log(pkgj.name + " " + pkgj.version + "\n\t" + pkgj.description);
+console.log("\tRun help to view application help. Run exit to quit.");
 
 handler.handle(argvs);
